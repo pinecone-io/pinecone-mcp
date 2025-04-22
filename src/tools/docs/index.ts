0ec71f6f876b3d31beb902e4611b19e84dec91d5
Jakey Fowler <jakey@pinecone.io>
2025-04-21 23:22:29 -0700

    use new sse client for each connection

--- src/tools/docs/index.ts
+++ src/tools/docs/index.ts
@@ -1,19 +1,6 @@
-import {Client} from '@modelcontextprotocol/sdk/client/index.js';
-import {SSEClientTransport} from '@modelcontextprotocol/sdk/client/sse.js';
 import {McpServer} from '@modelcontextprotocol/sdk/server/mcp.js';
-import {DOCS_SSE_URL} from '../../constants.js';
-import {PINECONE_MCP_VERSION} from '../../version.js';
 import {addSearchDocsTool} from './search-docs.js';
 
 export default async function addDocsTools(server: McpServer) {
-  const sseTransport = new SSEClientTransport(new URL(DOCS_SSE_URL));
-
-  const client = new Client({
-    name: 'pinecone-docs',
-    version: PINECONE_MCP_VERSION,
-  });
-
-  await client.connect(sseTransport);
-
-  addSearchDocsTool(server, client);
+  addSearchDocsTool(server);
 }
--- src/tools/docs/search-docs.ts
+++ src/tools/docs/search-docs.ts
@@ -1,6 +1,9 @@
 import {Client} from '@modelcontextprotocol/sdk/client/index.js';
+import {SSEClientTransport} from '@modelcontextprotocol/sdk/client/sse.js';
 import {McpServer} from '@modelcontextprotocol/sdk/server/mcp.js';
 import {z} from 'zod';
+import {DOCS_SSE_URL} from '../../constants.js';
+import {PINECONE_MCP_VERSION} from '../../version.js';
 
 const INSTRUCTIONS = 'Search Pinecone documentation for relevant information';
 
@@ -15,8 +18,17 @@ type SearchDocsResult = {
   }[];
 };
 
-export function addSearchDocsTool(server: McpServer, client: Client) {
+export function addSearchDocsTool(server: McpServer) {
   server.tool('search-docs', INSTRUCTIONS, SCHEMA, async ({query}) => {
+    const sseTransport = new SSEClientTransport(new URL(DOCS_SSE_URL));
+
+    const client = new Client({
+      name: 'pinecone-docs',
+      version: PINECONE_MCP_VERSION,
+    });
+
+    await client.connect(sseTransport);
+
     return (await client.callTool({
       name: 'get_context',
       arguments: {query},
